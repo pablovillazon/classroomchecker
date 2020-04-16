be5c349982e1da2e6456be4360c3931df5d6c61f
Pablo Villazon <p.villazon@gmail.com>
2020-04-16 19:58:31 -0400

    initial implementation

---
+++ src/main/java/be/jkin/classroomchecker/ClassroomCheckerApplication.java
@@ -0,0 +1,13 @@
+package be.jkin.classroomchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassroomCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/jkin/classroomchecker/ClassroomCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package be.jkin.classroomchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
